2df1b24f69f8ee262d04743280c17aab6cfddc30
dardan100 <nuredinidardan5@gmail.com>
2024-12-01 01:31:02 +0100

    Update TodayItem

--- src/features/check-in-out/TodayItem.jsx
+++ src/features/check-in-out/TodayItem.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import SpanStatus from "../../ui/SpanStatus";
 import { useNavigate } from "react-router-dom";
 import CheckoutButton from "./CheckoutButton";
 
 export default function TodayItem({ activity }) {
-  const { id, status, guests, numNights } = activity;
-  const { fullName } = guests;
+  const { id, status, guests = {}, numNights } = activity;
+  const fullName = guests?.fullName || "Unknown Guest"; // Fallback to "Unknown Guest" if guests or fullName is null
   const navigate = useNavigate();
+
   return (
     <div className="flex border-b border-b-gray-300 border-t border-t-gray-300 p-3 justify-between items-center w-full px-2 md:px-10">
       <div className="flex gap-1 md:gap-8 ">
@@ -19,8 +19,8 @@ export default function TodayItem({ activity }) {
           <p className="text-[12px] bg-gray-500 font-medium text-more-gray-100 rounded-3xl flex justify-center px-3 uppercase items-center">
             Departing
           </p>
-        )}{" "}
-        <p className=" whitespace-nowrap">{fullName}</p>
+        )}
+        <p className="whitespace-nowrap">{fullName}</p>
       </div>
       <div className="flex gap-4 md:gap-8 items-center">
         <p>{numNights} nights</p>
